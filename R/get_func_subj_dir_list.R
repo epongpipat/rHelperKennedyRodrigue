d4ddf611abb96f39ec080baad86cd6e8eace0a82
Ekarin Pongpipat <epongpipat@gmail.com>
2019-09-05 18:40:05 -0500

    Create get_func_subj_dir_list.R

---
+++ R/get_func_subj_dir_list.R
@@ -0,0 +1,19 @@
+get_func_subj_dir_list <- function(wave = 1) {
+  
+  if (!is.numeric(wave) || wave <= 0) {
+    stop(paste0("Wave (", wave,") must be a number greater than 0"))
+  }
+  
+  dir <- paste0(get_leading_path(), "KK_KR_JLBS/Wave", wave, "/MRI/FMRI/data/")
+  
+  if (!dir.exists(dir)) {
+    stop(paste0("directory (", dir,") does not exist"))
+  }
+  
+  files <- list.files(dir, "3tb*") %>%
+    str_subset(., ".mat", T)
+  
+  path <- paste0(dir, files)
+  
+  return(path)
+}
