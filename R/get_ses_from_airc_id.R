f96664dbab9d8f9fe1aba8bb01d2ef257c181bb9
Ekarin Eric Pongpipat <epongpipat@gmail.com>
2023-05-25 12:32:55 -0500

    update warning to include airc_id

--- R/get_ses_from_airc_id.R
+++ R/get_ses_from_airc_id.R
@@ -11,12 +11,12 @@ get_ses_from_airc_id <- function(airc_id) {
   df <- read.csv(in_ids) 
   idx <- which(df$mri_id == airc_id)
   if (length(idx) == 0) {
-    warning('no study ID found for this AIRC ID')
+    warning(glue('no study ID found for this AIRC ID ({airc_id})'))
     ses <- NA
   } else if (length(idx) == 1) {
     ses <- df[[idx, 'wave']]
   } else if (length(idx) > 1) {
-    warning('more than 1 session found for this AIRC ID')
+    warning(glue('more than 1 session found for this AIRC ID ({airc_id})'))
     ses <- NA
   }
   return(ses)
